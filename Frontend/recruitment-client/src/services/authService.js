daf2f50419f1509cc4f931265e114ddb6d381a8b
dewmi2003 <133262159+dewmi2003@users.noreply.github.com>
2026-07-23 14:21:12 +0530

    Fix frontend auth service duplicate api import

--- Frontend/recruitment-client/src/services/authService.js
+++ Frontend/recruitment-client/src/services/authService.js
@@ -1,29 +1,5 @@
 import api from "./api";
 
-
-export const loginUser = async (data) => {
-
-    const response = await api.post(
-        "/auth/login",
-        data
-    );
-
-    return response.data;
-};
-
-
-
-export const registerUser = async (data) => {
-
-    const response = await api.post(
-        "/auth/register",
-        data
-    );
-
-    return response.data;
-};
-import api from "./api";
-
 export const loginUser = async (data) => {
   const response = await api.post("/auth/login", data);
   return response.data;
